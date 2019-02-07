1c9279644a93d4f52ec79c87346a1b8edb842184
compiled-writer-1970 <kevypark@gmail.com>
2019-02-07 17:43:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+function theBeatesPlay(musicians, instruments) {
+  var arr = [];
+  for(var i=0;i<musicians.length;i++) {
+    arr.push(musicians[i] + ' play ' + instruments[i]);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts) {
+  var arr = [];
+  var i = 0;
+  while (i<facts.length) {
+    arr.push(facts[i] + '!');
+    i++
+  }
+  return arr;
+}
+
+function iLoveTheBeatles(num) {
+  var arr = [];
+  var i=0; 
+  do{
+    arr.push('I love the Beatles!');
+    i++;
+  }
+  while(i<num && i<15);
+  return arr;
+}
+
